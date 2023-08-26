80f88a8f7b45a4ac40bb7c5a60c8c06d9b60f278
eshonqul <abdulazizoveshonqul66@gmail.com>
2023-08-27 01:53:22 +0500

    role add

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export {default as StudentLogin} from "./StudenLogin";
-export {default as AdminLogin} from "./AdminLogin";
-export {default as TeacherLogin} from "./TeacherLogin";
-export {default as Staff} from "./Staff";
-export {default as Support} from "./Support";
-export {default as Home} from "./Home";
